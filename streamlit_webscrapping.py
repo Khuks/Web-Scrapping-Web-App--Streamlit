e496111ac2984f161d55966cb4654b10ad1d0ea6
Khuks <khulekanimkholo30@gmail.com>
2023-03-30 11:55:03 +0200

    First Commit

--- streamlit_webscrapping.py
+++ streamlit_webscrapping.py
@@ -20,7 +20,6 @@ content =  BeautifulSoup(res.content,'html.parser')
 quotes = content.find_all('div',class_='quote')
 
 quote_file = []
-
 for quote in quotes:
     text = quote.find('span',class_ ='text').text
     author = quote.find('small',class_ = 'author').text
@@ -30,7 +29,7 @@ for quote in quotes:
     st.markdown(f"<a href=https://quotes.toscrape.com{link['href']}>{author}</a>",unsafe_allow_html=True)
     st.code(f"https://quotes.toscrape.com{link['href']}")
     quote_file.append([text,author,link['href']])
-
+    
 if generate:
     try:
       df = pd.DataFrame(quote_file)
